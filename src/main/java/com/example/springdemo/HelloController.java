fb4bacea9b850757cea427383985fb5ac1f0cad9
AskC <andycheung_@hotmail.com>
2023-01-05 12:04:49 +0100

    Added Thymeleaf as example to build html + css from server side.

--- src/main/java/com/example/springdemo/HelloController.java
+++ src/main/java/com/example/springdemo/HelloController.java
@@ -1,20 +1,23 @@
 package com.example.springdemo;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot! "
+    public String index(Model model) {
+        String greeting = "Greetings from Spring Boot! "
                 + StringUtils.leftPad(String.valueOf(LocalDateTime.now().getHour()), 2, "0") + ":"
                 + StringUtils.leftPad(String.valueOf(LocalDateTime.now().getMinute()), 2, "0") + ":"
                 + StringUtils.leftPad(String.valueOf(LocalDateTime.now().getSecond()), 2, "0");
+        model.addAttribute("date", greeting);
+        return "hello";
     }
 
     @GetMapping("/workout")
