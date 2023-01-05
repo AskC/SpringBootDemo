172541674187af819b94be396176c8d5df8732ab
AskC <andycheung_@hotmail.com>
2023-01-05 14:02:13 +0100

    Added controller to test call to external API and expose in own endpoints.

---
+++ src/main/java/com/example/springdemo/controller/RetrieveDataController.java
@@ -0,0 +1,24 @@
+package com.example.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/info")
+public class RetrieveDataController {
+
+    @GetMapping(value = "/country")
+    public List<Object> getCountry() {
+        String url = "https://restcountries.com/v3.1/name/netherlands";
+        RestTemplate restTemplate = new RestTemplate();
+        Object[] country = restTemplate.getForObject(url, Object[].class);
+
+        return Arrays.asList(country);
+    }
+
+}
